aa2c6b639cfaa8974c19a035fe786d671c13ea7b
dreamsystemtech <60945265+dreamsystemtech@users.noreply.github.com>
2022-11-28 18:46:39 +0300

    comit 3

--- First/nums.py
+++ First/nums.py
@@ -1,9 +1,11 @@
-def plus (a,b):
+def plus(a, b):
     return a+b
 
-def minus (a,b):
+
+def minus(a, b):
     return a-b
 
+
 if __name__ == '__main__':
-    print(plus(5,6))
-    print(minus(9,8))
+    print(plus(5, 6))
+    print(minus(9, 8))
