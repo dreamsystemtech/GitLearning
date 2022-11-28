90ef0025d08e1270452c671cc18beee96f143425
dreamsystemtech <60945265+dreamsystemtech@users.noreply.github.com>
2022-11-28 20:03:50 +0300

    comit 5 namesum feature

---
+++ First/namesum.py
@@ -0,0 +1,9 @@
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя'
+Alphabet = alphabet.upper()
+
+lettersvalue = {value: key for key, value in enumerate(alphabet.split(), 1)}
+Lettersvalue = {value: key for key, value in enumerate(Alphabet.split(), 1)}
+
+name = "Виктор"
+
+namesum = 0
